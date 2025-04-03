6472b0ca709692d243cd1db0d215ac35358a6e50
santhoshm-rapyder <santhosh.m@rapyder.com>
2025-04-03 12:26:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "environment" {
+  description = "The environment name (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+}
+
+variable "create_nat_gateway" {
+  description = "Flag to create NAT Gateway (true for single NAT, false for multi-NAT)"
+  type        = bool
+  default     = true
+}
